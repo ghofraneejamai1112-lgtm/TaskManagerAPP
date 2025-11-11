cdd1e9be65bd09e6106fb7cda08bec5965473cfa
ghofrane <ghofraneejamai1112.com>
2025-11-11 10:23:02 +0100

    add

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal ,Input} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from "./task/task";
+import { Menu } from "./menu/menu";
+import { Nav } from "./nav/nav";
+import { Aside } from "./aside/aside";
+import { Footer } from "./footer/footer";
+import { Section } from "./section/section";
+import { CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+
+import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
+import { Taskservice } from './taskservice';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //
+  imports: [RouterOutlet, Task, Menu, Nav, Aside, Footer, Section,UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,CurrencyPipe,PercentPipe,DecimalPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+//
 export class App {
-  protected readonly title = signal('todolist');
+  today = new Date();
+  price = 1234.56;
+percentage = 0.259;
+ tasks: any[] = [];
+ constructor(private taskService: Taskservice) {}
+ ngOnInit() {
+this.tasks = this.taskService.getTasks();
 }
+
+markAdd(){
+  this.tasks.push({ id:'4',title: 'new task', done: false} )
+}
+deleTsk(ind:number){
+  this.tasks.splice(ind ,1)
+}
+}
+
